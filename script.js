6e23cd695148db28070948f5f16a1aabca2276dd
Lucas da Silva Dias <lucasdias.ld10@gmail.com>
2022-10-22 16:46:49 -0300

    inicio implementação javascript e virada de carta

---
+++ script.js
@@ -0,0 +1,7 @@
+const cartas = ['./img/bobrossparrot.gif', './img/bobrossparrot.gif',
+                './img/explodyparrot.gif', './img/explodyparrot.gif',
+                './img/fiestaparrot.gif', './img/fiestaparrot.gif',
+                './img/metalparrot.gif', './img/metalsparrot.gif',
+                './img/revertitparrot.gif', './img/revertitossparrot.gif',
+                './img/unicornparrot.gif', './img/unicornparrot.gif']
+
